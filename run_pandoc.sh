727db10757335e997c167614a3bf08a6ca3dc933
Mo Kweon <kkweon@google.com>
2020-10-08 02:09:04 -0700

    Setup Pandoc (#22)
    * Setup Pandoc
    
    * Add pandoc deployment.yaml
    
    * Add Deployment badge to README
    
    * Fix whitespace in title

---
+++ run_pandoc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+rm -rf build && mkdir build
+
+pandoc \
+  --to html5 \
+  --standalone \
+  --table-of-contents \
+  --toc-depth=4 \
+  --highlight-style pygments \
+  --self-contained \
+  --resource-path=diagrams \
+  -o build/index.html \
+  title.txt \
+  5-20/5-20.md \
+  20-37/20-37.md \
+  37-55/37-55.md \
+  55-70/55-70.md \
+  70-89/70-89.md \
+  89-101/89-101.md \
+  101-114/101-114.md \
+  114-128/114-128.md \
+  128-139/128-139.md \
+  139-164/139-164.md \
+  164-173/164-173.md \
+  173-191/173-191.md \
+  191-199/191-199.md
